e060d8a68b8a6cd672a3c46eb031b3581199fc80
ChermDev <carlos_e8246@hotmail.com>
2023-02-18 03:37:39 -0600

    replace category type with string

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -14,4 +14,14 @@ class Product(BaseModel):
     full_name: str
     price: float
     image: str
-    category: Category
+    category: str
+
+
+class Score(BaseModel):
+    id: int
+    created_at: str
+    updated_at: str
+    username: str
+    score: dict
+    name: str = None
+    avatar: str = None
