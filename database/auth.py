91999334ea111fd35a6b9ef2d9c0d7dcc2c22e4a
ChermDev <carlos_e8246@hotmail.com>
2023-03-07 17:05:35 -0600

    Add jwtBearer

--- database/auth.py
+++ database/auth.py
@@ -1,10 +1,45 @@
+import urllib.parse
 from database.db import DB
+from fastapi import Request
+from supabase import Client
+from database.db import DB
+from fastapi import HTTPException
 from gotrue.types import AuthResponse
 from gotrue.types import OAuthResponse
-import urllib.parse
+from gotrue.errors import AuthApiError
+from fastapi.security import HTTPBearer
+from fastapi.security import HTTPAuthorizationCredentials
 
 
-# TODO: not complete, initial commit.
+class jwtBearer(HTTPBearer):
+
+    def __init__(self, auto_error: bool = True) -> None:
+        super(jwtBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super(
+            jwtBearer, self).__call__(request)
+        if credentials:
+            if not credentials.scheme == "Bearer":
+                raise HTTPException(
+                    status_code=403, detail="Invalid or Expired Token!")
+            return self.verify_jwt(credentials.credentials)
+        else:
+            raise HTTPException(
+                status_code=403, detail="Invalid or Expired Token!")
+
+    def verify_jwt(self, jwt_token: str) -> Client:
+        try:
+            supabase = DB().supabase
+            # supabase.auth.get_user(jwt_token)
+            supabase.postgrest.auth(jwt_token)
+            return supabase
+        except AuthApiError as err:
+            raise HTTPException(
+                status_code=400,
+                detail=str(err))
+
+
 class User():
 
     def __init__(self) -> None:
