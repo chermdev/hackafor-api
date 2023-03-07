797f2166de6b062ab63beea462c8d04db9aa6893
ChermDev <carlos_e8246@hotmail.com>
2023-03-07 02:18:01 -0600

    Add ranking route

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routes.auth import auth_router
 from routes.user import user_router
+from routes.ranking import ranking_router
 from routes.products import products_router
 from starlette.responses import RedirectResponse
 
@@ -9,6 +10,7 @@ app = FastAPI()
 
 app.include_router(auth_router)
 app.include_router(user_router)
+app.include_router(ranking_router)
 app.include_router(products_router)
 
 
---
+++ routes/ranking.py
@@ -0,0 +1,31 @@
+from database.db import DB
+from fastapi import Request
+from fastapi import APIRouter
+from pydantic import BaseModel
+from fastapi import HTTPException
+from gotrue.errors import AuthApiError
+from postgrest.exceptions import APIError
+
+
+class Ranking(BaseModel):
+    id: int | None
+    created_at: str | None
+    username: str | None
+    score: int
+    data: dict | None
+
+
+ranking_router = APIRouter(prefix="/ranking")
+
+
+@ranking_router.get("/")
+def get_user_info(max_results: str = 10) -> list[dict]:
+    try:
+        supabase = DB().supabase
+        response = supabase.table("ranking").select("*").order("score",
+                                                               desc=True).execute()
+        return response.data[:max_results]
+    except AuthApiError as err:
+        raise HTTPException(
+            status_code=400,
+            detail=str(err))
