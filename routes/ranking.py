08d68ab0de31052bc5c97e3aa656cb36d4525dd1
ChermDev <carlos_e8246@hotmail.com>
2023-03-07 19:39:25 -0600

    Update ranking return linter

--- routes/ranking.py
+++ routes/ranking.py
@@ -9,6 +9,7 @@ from postgrest.exceptions import APIError
 
 class Ranking(BaseModel):
     id: int | None
+    rank: str
     created_at: str | None
     username: str | None
     score: int
@@ -19,7 +20,7 @@ ranking_router = APIRouter(prefix="/ranking")
 
 
 @ranking_router.get("/")
-def get_ranking_list(max_results: int = 10) -> list[dict]:
+def get_ranking_list(max_results: int = 10) -> list[Ranking]:
     try:
         supabase = DB().supabase
         response = supabase.table("ranking").select("*").order("score",
