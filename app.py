5515b98925195d0396e3c21816fe39044df0f2fe
ChermDev <carlos_e8246@hotmail.com>
2023-03-07 20:01:48 -0600

    Add CORS

--- app.py
+++ app.py
@@ -4,9 +4,19 @@ from routes.user import user_router
 from routes.ranking import ranking_router
 from routes.products import products_router
 from starlette.responses import RedirectResponse
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(user_router)
