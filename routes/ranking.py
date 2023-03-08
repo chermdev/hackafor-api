bf3524c6ed5c073ed238c252c5fd48edbb158ceb
ChermDev <carlos_e8246@hotmail.com>
2023-03-07 19:15:46 -0600

    Fix ranking error using str instead of int

--- routes/ranking.py
+++ routes/ranking.py
@@ -19,7 +19,7 @@ ranking_router = APIRouter(prefix="/ranking")
 
 
 @ranking_router.get("/")
-def get_ranking_list(max_results: str = 10) -> list[dict]:
+def get_ranking_list(max_results: int = 10) -> list[dict]:
     try:
         supabase = DB().supabase
         response = supabase.table("ranking").select("*").order("score",
